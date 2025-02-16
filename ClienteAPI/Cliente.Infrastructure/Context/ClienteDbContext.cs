23f45794bdc611b0c4061b25e94b43c4036f9ea7
Dennys Fonseca <dennys_ps_@LIVE.COM>
2025-02-15 23:01:23 -0300

    feat: projeto inicial

---
+++ ClienteAPI/Cliente.Infrastructure/Context/ClienteDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cliente.Infrastructure.Context;
+
+public class ClienteDbContext(DbContextOptions<ClienteDbContext> options) : DbContext(options)
+{
+    public DbSet<Cliente> Clientes { get; set; }
+
+}
---
+++ ClienteAPI/ClienteAPI/Controllers/ClienteController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClienteAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClienteController : ControllerBase
+    {
+       
+    }
+}
