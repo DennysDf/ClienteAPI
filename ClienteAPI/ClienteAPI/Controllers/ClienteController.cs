76e4b752eca8f295ba73d4edfac926c5e9f9e249
Dennys Fonseca <dennys_ps_@LIVE.COM>
2025-02-17 05:09:49 -0300

    feat: ajuste nas rotas

--- ClienteAPI/ClienteAPI/Controllers/ClienteController.cs
+++ ClienteAPI/ClienteAPI/Controllers/ClienteController.cs
@@ -1,78 +1,81 @@
 using ClienteAPI.Application.DTOs;
 using ClienteAPI.Application.Services.Interfaces;
 using ClienteAPI.Infrastructure.Repositories.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ClienteAPI.Controllers
+namespace ClienteAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]    
+public class ClienteController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class ClienteController : ControllerBase
+    private readonly IClienteService _clienteService;
+
+    public ClienteController(IClienteService clienteService)
     {
-        private readonly IClienteService _clienteService;
+        _clienteService = clienteService;
+    }
 
-        public ClienteController(IClienteService clienteService)
+    [Authorize]
+    [HttpGet("Obter")]
+    public async Task<ActionResult<IEnumerable<ClienteDTO>>> GetCliente()
+    {
+        var clientes = await _clienteService.GetClientes();
+        return Ok(clientes);
+    }
+
+    [Authorize]
+    [HttpGet( "Obter/{id}")]
+    public async Task<ActionResult<ClienteDTO>> Get(int id)
+    {
+        var cliente = await _clienteService.GetById(id);
+
+        if (cliente == null)
         {
-            _clienteService = clienteService;
+            return NotFound();
         }
-        
-        [HttpGet("Obter")]
-        public async Task<ActionResult<IEnumerable<ClienteDTO>>> Get()
+        return Ok(cliente);
+    }
+
+    [Authorize]
+    [HttpPost("Cadastrar")]
+    public async Task<ActionResult> Post([FromBody] ClienteDTO clienteDTO)
+    {
+        if (!ModelState.IsValid)
         {
-            var clientes = await _clienteService.GetClientes();
-            return Ok(clientes);
-        }
-
-        [HttpGet( "ObterCliente/{id}")]
-        public async Task<ActionResult<ClienteDTO>> Get(int id)
-        {
-            var cliente = await _clienteService.GetById(id);
-
-            if (cliente == null)
-            {
-                return NotFound();
-            }
-            return Ok(cliente);
-        }
-
-        [HttpPost("Cadastrar")]
-        public async Task<ActionResult> Post([FromBody] ClienteDTO clienteDTO)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            await _clienteService.Add(clienteDTO);
-
-            return new CreatedAtRouteResult("GetCliente",
-                new { id = clienteDTO.Id }, clienteDTO);
+            return BadRequest(ModelState);
         }
 
-        [HttpPut("Atualizar/{id}")]
-        public async Task<ActionResult> Put(int id, [FromBody] ClienteDTO clienteDTO)
+        await _clienteService.Add(clienteDTO);
+
+        return Ok(clienteDTO);
+    }
+
+    [Authorize]
+    [HttpPut("Atualizar/{id}")]
+    public async Task<ActionResult> Put(int id, [FromBody] ClienteDTO clienteDTO)
+    {
+        if (id != clienteDTO.Id)
         {
-            if (id != clienteDTO.Id)
-            {
-                return BadRequest();
-            }
-
-            await _clienteService.Update(clienteDTO);
-
-            return Ok(clienteDTO);
+            return BadRequest();
         }
 
-        [HttpDelete("Delete/{id}")]
-        public async Task<ActionResult<ClienteDTO>> Delete(int id)
-        {
-            var produtoDto = await _clienteService.GetById(id);
-            if (produtoDto == null)
-            {
-                return NotFound();
-            }
-            await _clienteService.Delete(id);
-            return Ok(produtoDto);
-        }
+        await _clienteService.Update(clienteDTO);
 
+        return Ok(clienteDTO);
+    }
+
+    [Authorize]
+    [HttpDelete("Deletar/{id}")]
+    public async Task<ActionResult<ClienteDTO>> Delete(int id)
+    {
+        var produtoDto = await _clienteService.GetById(id);
+        if (produtoDto == null)
+        {
+            return NotFound();
+        }
+        await _clienteService.Delete(id);
+        return Ok(produtoDto);
     }
 }
